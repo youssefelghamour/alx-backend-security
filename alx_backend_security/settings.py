f833f9477d3443b90b29799466ad60dd2572309f
youssef <youssefelghamour@gmail.com>
2026-01-17 11:31:26 +0100

    Rate Limiting by IP

--- alx_backend_security/settings.py
+++ alx_backend_security/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'django_ratelimit',
     'ip_tracking',
 ]
 
@@ -119,3 +120,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
--- ip_tracking/views.py
+++ ip_tracking/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django_ratelimit.decorators import ratelimit
 
-# Create your views here.
+
+@ratelimit(key='user', rate='10/m', block=False)
+@ratelimit(key='ip', rate='5/m', block=False)
+def login_view(request):
+    """Views for login with rate limiting applied
+        - User-based limit (authenticated): 10 requests per minute
+        - IP-based limit (anonymous): 5 requests per minute
+    """
+    # Check if the request was limited to handle the Ratelimited exception manually
+    # Or you can let the decorator handle it by setting block=True which stops the request automatically
+    if getattr(request, 'limited', False):
+        return HttpResponse("Too many requests", status=429)
+    return HttpResponse("Ok")
